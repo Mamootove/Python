5bf2a1ca02a8c8d7e5b80ed6e2cd2b60285a286c
Mamootove <mahanmahmoodov@gamil.com>
2025-11-01 22:15:43 +0330

    commit

--- proj2_uni.py
+++ proj2_uni.py
@@ -1,19 +1,33 @@
 def take_name():
+    '''
+    Get user name and  surname, Then retuns them.
+    '''
     name = input("Tell me your name: ")
     sur = input("Tell me your last name: ")
     return name, sur
 
 def len_name(name, sur):
+    '''
+    just make len of st and then return it and it's len.
+    '''
     ln = len(name)
     ls = len(sur)
     return name, sur, ln, ls
 
 def khoshgelesh_kon(name, sur, ln, ls):
+    '''
+    Prints using f-string and match the name with it len. 
+    '''
     print(f"Name: {name}, len: {ln}")
     print(f"Surname: {sur}, len: {ls}")
     print(f"{name} {sur}, total: {ln+ ls}")
 
 def rate_hour():
+    '''
+    A liitle more complicated than others!
+    get 2 inputes and multiplies them 
+    if even one of the inputes isn't int, it give you an error with the describtion that where it happend.  
+    '''
     hour = input("How many hour do you work a day? ")
     rate = input("How much do you earn in an hour? ")
     a = True

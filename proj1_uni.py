80859a1b16297ea66864fff760a8e7dd6af794fc
Mahan <mahan.mahmoodi.king@gmail.com>
2025-11-01 22:08:01 +0330

    Update proj1_uni.py

--- proj1_uni.py
+++ proj1_uni.py
@@ -1,15 +1,19 @@
 name = input("Enter your name: ")
 sur = input("Enter your sur name: ")
-age = int(input("How old are you? "))
+
 ln = len(name)
 ls = len(sur)
 tot = ln + ls
+
 print(f"{name} has {ln} characters!", end="***")
 print(f"{sur} has {ls} characters!")
 print(f"the total characters are {tot}")
+
 print("________________________________________")
+
 hour = int(input("How many hours do you work a day? "))
 pay = int(input("How much do you earn an hour? "))
 #Probably user work 22 days a month(not expecting thursdays and fridays)
 money = hour * pay *22
+
 print(f"you probably earn {money} monthly.")
